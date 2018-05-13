d69e71b80c38ceba3068e257f419f0beb75e319a
candr006 <candr006@ucr.edu>
2018-05-13 22:47:06 +0000

    basic block code from example added

--- Workspace/llvm/lib/Transforms/CS201PathProfiling/CS201PathProfiling.cpp
+++ Workspace/llvm/lib/Transforms/CS201PathProfiling/CS201PathProfiling.cpp
@@ -10,27 +10,99 @@
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/Type.h"
+#include <iostream>
+#include <string>
 
 using namespace llvm;
 
 namespace {
+
+  static Function* printf_prototype(LLVMContext& ctx, Module *mod) {
+    std::vector<Type*> printf_arg_types;
+    printf_arg_types.push_back(Type::getInt8PtrTy(ctx));
+ 
+    FunctionType* printf_type = FunctionType::get(Type::getInt32Ty(ctx), printf_arg_types, true);
+    Function *func = mod->getFunction("printf");
+    if(!func)
+      func = Function::Create(printf_type, Function::ExternalLinkage, Twine("printf"), mod);
+    func->setCallingConv(CallingConv::C);
+    return func;
+  }
+
   struct CS201PathProfiling : public FunctionPass {
   static char ID;
   CS201PathProfiling() : FunctionPass(ID) {}
+	LLVMContext *Context;
 
+    GlobalVariable *bbCounter = NULL;
+    GlobalVariable *BasicBlockPrintfFormatStr = NULL;
+    Function *printf_func = NULL;
+ 
     //----------------------------------
     bool doInitialization(Module &M) {
-      return false;
+      errs() << "\n---------Starting BasicBlockDemo---------\n";
+      Context = &M.getContext();
+      bbCounter = new GlobalVariable(M, Type::getInt32Ty(*Context), false, GlobalValue::InternalLinkage, ConstantInt::get(Type::getInt32Ty(*Context), 0), "bbCounter");
+      const char *finalPrintString = "BB Count: %d\n";
+      Constant *format_const = ConstantDataArray::getString(*Context, finalPrintString);
+      BasicBlockPrintfFormatStr = new GlobalVariable(M, llvm::ArrayType::get(llvm::IntegerType::get(*Context, 8), strlen(finalPrintString)+1), true, llvm::GlobalValue::PrivateLinkage, format_const, "BasicBlockPrintfFormatStr");
+      printf_func = printf_prototype(*Context, &M);
+ 
+      errs() << "Module: " << M.getName() << "\n";
+ 
+      return true;
     }
-
+ 
     //----------------------------------
     bool doFinalization(Module &M) {
+      errs() << "-------Finished BasicBlocksDemo----------\n";
+ 
       return false;
     }
     
     //----------------------------------
     bool runOnFunction(Function &F) override {
+      errs() << "Function: " << F.getName() << '\n';
+ 
+      for(auto &BB: F) {
+        // Add the footer to Main's BB containing the return 0; statement BEFORE calling runOnBasicBlock
+        if(F.getName().equals("main") && isa<ReturnInst>(BB.getTerminator())) { // major hack?
+          addFinalPrintf(BB, Context, bbCounter, BasicBlockPrintfFormatStr, printf_func);
+        }
+        runOnBasicBlock(BB);
+      }
+ 
+      return true; 
+    }
+
+    bool runOnBasicBlock(BasicBlock &BB) {
+      errs() << "BasicBlock: " << BB.getName() << '\n';
+      IRBuilder<> IRB(BB.getFirstInsertionPt()); // Will insert the generated instructions BEFORE the first BB instruction
+ 
+      Value *loadAddr = IRB.CreateLoad(bbCounter);
+      Value *addAddr = IRB.CreateAdd(ConstantInt::get(Type::getInt32Ty(*Context), 1), loadAddr);
+      IRB.CreateStore(addAddr, bbCounter);
+ 
+      for(auto &I: BB)
+        errs() << I << "\n";
+ 
       return true;
+    }
+
+     //----------------------------------
+    // Rest of this code is needed to: printf("%d\n", bbCounter); to the end of main, just BEFORE the return statement
+    // For this, prepare the SCCGraph, and append to last BB?
+    void addFinalPrintf(BasicBlock& BB, LLVMContext *Context, GlobalVariable *bbCounter, GlobalVariable *var, Function *printf_func) {
+      IRBuilder<> builder(BB.getTerminator()); // Insert BEFORE the final statement
+      std::vector<Constant*> indices;
+      Constant *zero = Constant::getNullValue(IntegerType::getInt32Ty(*Context));
+      indices.push_back(zero);
+      indices.push_back(zero);
+      Constant *var_ref = ConstantExpr::getGetElementPtr(var, indices);
+ 
+      Value *bbc = builder.CreateLoad(bbCounter);
+      CallInst *call = builder.CreateCall2(printf_func, var_ref, bbc);
+      call->setTailCall(false);
     }
 
   };
